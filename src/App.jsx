c8cf1c97c1162308bea2be7965309b88515f4ad9
HeersareAI <Heersareai@gmail.com>
2026-05-19 00:38:19 +0200

    exercise01

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import UserCard from "./UserCard";
+ 
+function App() {
+  return (
+    <div>
+      <UserCard name="John Doe" email="john.doe@example.com" />
+    </div>
+  );
+}
+ 
+export default App;
